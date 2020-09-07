3d0b2e85f1e3d879365d92878de3264c8ab2ab51
douglas-cpp <douglasc.dev@gmail.com>
2020-09-07 14:07:18 -0300

    Darker colors for snake and food

--- snake/constants.py
+++ snake/constants.py
@@ -1,14 +1,15 @@
 from enum import Enum
 from pygame.rect import Rect
+from pygame.color import Color
 
 
 class Color:
-    WHITE = (255, 255, 255)
-    BLACK = (0, 0, 0)
-    GREEN = (0, 255, 0)
-    RED = (255, 0, 0)
-    GREY = (50, 50, 50)
-    LIGHT_GREY = (60, 60, 60)
+    WHITE = Color(200, 200, 200)
+    BLACK = Color(0, 0, 0)
+    GREEN = Color(0, 200, 0)
+    RED = Color(230, 0, 0)
+    GREY = Color(50, 50, 50)
+    LIGHT_GREY = Color(60, 60, 60)
 
 
 class Game:
